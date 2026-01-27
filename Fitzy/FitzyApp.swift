d26c971d5ec0abb71c36626dd31c3be5cf0fa213
cgrtrz <cagri_terzi@hotmail.com>
2026-01-27 11:51:37 +0300

    Initial Commit

---
+++ Fitzy/FitzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitzyApp.swift
+//  Fitzy
+//
+//  Created by Cagri Terzi on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
